58c25567f41c890e973ae1a1b9b7d466fb5f9d33
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-14 19:33:32 -0500

    MongoDB connection

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/merndb')
+    
+    console.log('>>> DB is connected');
+  } catch (error) {
+    console.log(error)
+  }
+}
