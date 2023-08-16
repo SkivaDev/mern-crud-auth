4cf07cd839ededfed7bc11627f17a1f8cd9e1153
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-16 18:09:10 -0500

    Creacion del Token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { createAccessToken } from '../libs/jwt.js';
 
 export const register = async (req, res) => { 
  
@@ -6,15 +8,33 @@ export const register = async (req, res) => {
 
   
   try {
-    const newUser = new User({ username, email, password })
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
+
+    const newUser = new User({ 
+      username, 
+      email, 
+      password: passwordHash
+    })
  
     const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id })
 
-    res.status(201).json(userSaved);
+
+    res.cookie('token', token)
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt
+    })
+
   } catch (error) {
-    console.log(error)    
+    res.status(400).json({ message: error.message });
   }
 
-  res.send('Registrando') 
+  res.send('Registrando')
 };
 export const login = (req, res) => { res.send('login') };
