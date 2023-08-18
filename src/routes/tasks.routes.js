eb890749e8093cf4bcba27ad8548e76597f8275d
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-17 19:27:18 -0500

    Validacion de datos

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,23 +1,30 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
-import { 
+import {
   getTasks,
   createTask,
   getTask,
   updateTask,
-  deleteTask
+  deleteTask,
 } from "../controllers/tasks.controller.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 
 const router = Router();
 
-router.get('/tasks', authRequired, getTasks);
+router.get("/tasks", authRequired, getTasks);
 
-router.get('/tasks/:id', authRequired, getTask);
+router.get("/tasks/:id", authRequired, getTask);
 
-router.post('/tasks', authRequired, createTask);
+router.post(
+  "/tasks",
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTask
+);
 
-router.put('/tasks/:id', authRequired, updateTask);
+router.put("/tasks/:id", authRequired, updateTask);
 
-router.delete('/tasks/:id', authRequired, deleteTask);
+router.delete("/tasks/:id", authRequired, deleteTask);
 
 export default router;
